94f101801aefa1eac2d39385d98fb1afc304716d
mjingram <micha.j.ingram@gmail.com>
2021-04-25 19:25:15 -0600

    complete objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,12 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+
+    num1 = 15.0;
+    num2 = 6.0;
+    num3 = 2.0;
+    average = (num1+num2+num3)/3;
+
+    System.out.println(average);
+  }
+}
