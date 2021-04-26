902949ab38990f8b6b2119a852f99bdcd8acebfd
mjingram <micha.j.ingram@gmail.com>
2021-04-25 19:19:02 -0600

    complete objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Michael Ingram";
+    int age = 27;
+    String state = "Missouri";
+
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
