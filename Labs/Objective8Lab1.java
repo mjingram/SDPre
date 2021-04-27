b6537e5cc074eca73c3d4db4370fb90a5ae0395b
mjingram <micha.j.ingram@gmail.com>
2021-04-27 16:17:59 -0600

    complete objective8Lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,13 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+    //TODO Write a for loop that will print the numbers 1-10
+    for(int i = 1; i<=10; i+=1){
+      System.out.println(i);
+    }
+    //TODO Write another for loop that will print the numbers 10-1.
+    for(int j = 10; j>=1; j-=1){
+      System.out.println(j);
+    }
+
+  }
+}
