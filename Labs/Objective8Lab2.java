07ddd454f55e38e7f1f0a4d26cfb003e7fc2ccb0
mjingram <micha.j.ingram@gmail.com>
2021-04-27 16:24:50 -0600

    complete objective8Lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,10 @@
+public class Objective8Lab2{
+  public static void main(String[] args){
+    int currentSum = 0;
+
+    for(int i = 1; i<=20; i+=1){
+      currentSum += i;
+    }
+    System.out.println(currentSum);
+  }
+}
