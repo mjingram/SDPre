35b32027f5c0bf2b515d4d60bb36c253d01efb80
mjingram <micha.j.ingram@gmail.com>
2021-04-27 15:34:08 -0600

    complete objective7Lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,11 @@
+public class Objective7Lab4{
+  public static void main(String[] args){
+    int count = 1;
+    int currentSum = 0;
+    while(count<=20){
+      currentSum += count;
+      count += 1;
+    }
+    System.out.println(currentSum);
+  }
+}
